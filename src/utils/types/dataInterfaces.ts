a779ad21e86ce65710cb10b5d8fea739922004c1
byNico1 <nicofoque0205@gmail.com>
2024-06-15 12:15:35 -0500

    add types for the react states containing person data

---
+++ src/utils/types/dataInterfaces.ts
@@ -0,0 +1,41 @@
+import { Dispatch, SetStateAction } from "react";
+
+export interface CommonPersonData {
+  id: string;
+  closed: boolean;
+}
+
+export interface PersonWorkData extends CommonPersonData {
+  companyName: string;
+  positionTitle: string;
+  responsabilities: string;
+  startDateJob: string;
+  endDateJob: string;
+}
+
+// Define the type for the education data
+export interface PersonEducationData extends CommonPersonData {
+  schoolName: string;
+  degree: string;
+  startDateEducation: string;
+  endDateEducation: string;
+  location: string;
+}
+
+export type PersonDataOptions = PersonWorkData | PersonEducationData;
+
+interface GeneralInfo {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export interface PersonFullDataInterface {
+  generalInfo: GeneralInfo;
+  schools: Omit<PersonEducationData[], "closed">;
+  companys: Omit<PersonWorkData[], "closed">;
+}
+
+export type PersonDataState<
+  T extends PersonFullDataInterface | PersonDataOptions
+> = [T, Dispatch<SetStateAction<T>>];
